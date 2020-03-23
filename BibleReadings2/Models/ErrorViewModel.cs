ca2af3a24aaedf01528b085593ecc5ce78149350
Andy Schott <andy@andyschott.com>
2020-03-22 21:37:01 -0400

    enable nullable reference types

--- BibleReadings2/Models/ErrorViewModel.cs
+++ BibleReadings2/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace BibleReadings2.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
