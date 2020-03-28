042e1c5b3aebb1249f83440d378155f893089de4
Andy Schott <andy@andyschott.com>
2020-03-28 18:27:19 -0400

    remove privacy policy

--- BibleReadings2/Controllers/HomeController.cs
+++ BibleReadings2/Controllers/HomeController.cs
@@ -19,10 +19,6 @@ namespace BibleReadings2.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
