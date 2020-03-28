8f56320f6ed119746e9d86a7390bb82724495742
Andy Schott <andy@andyschott.com>
2020-03-28 16:10:34 -0400

    serialize enums as strings

--- BibleReadings2/Startup.cs
+++ BibleReadings2/Startup.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,7 +20,11 @@ namespace BibleReadings2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddJsonOptions(config =>
+                {
+                    config.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
 
             services.AddJsonRepository();
 
